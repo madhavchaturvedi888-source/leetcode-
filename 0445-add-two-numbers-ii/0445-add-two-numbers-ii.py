5cc50e3e0b79666c466dd07c23fcfd96cca9b301
madhavchaturvedi888-source <madhav.chaturvedi888@gmail.com>
2026-08-31 10:20:32 +0530

    Time: 3 ms (78.16%), Space: 19.3 MB (81.85%) - LeetHub

---
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.py
@@ -0,0 +1,31 @@
+class Solution:
+    def addTwoNumbers(self, l1, l2):
+        stack1 = []
+        stack2 = []
+
+        
+        while l1:
+            stack1.append(l1.val)
+            l1 = l1.next
+
+        while l2:
+            stack2.append(l2.val)
+            l2 = l2.next
+
+        carry = 0
+        head = None
+
+         
+        while stack1 or stack2 or carry:
+            a = stack1.pop() if stack1 else 0
+            b = stack2.pop() if stack2 else 0
+
+            total = a + b + carry
+            carry = total // 10
+            digit = total % 10
+
+            new_node = ListNode(digit)
+            new_node.next = head
+            head = new_node
+
+        return head
