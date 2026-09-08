01284f7119afbf44985f65590b3f1405aeca6775
madhavchaturvedi888-source <madhav.chaturvedi888@gmail.com>
2026-09-08 16:23:56 +0530

    Time: 279 ms (20.61%), Space: 27.9 MB (65.7%) - LeetHub

---
+++ 0648-replace-words/0648-replace-words.py
@@ -0,0 +1,20 @@
+class Solution:
+    def replaceWords(self, dictionary: list[str], sentence: str) -> str:
+        roots = set(dictionary)
+        words = sentence.split()
+
+        result = []
+
+        for word in words:
+            replacement = word
+
+            for i in range(1, len(word) + 1):
+                prefix = word[:i]
+
+                if prefix in roots:
+                    replacement = prefix
+                    break
+
+            result.append(replacement)
+
+        return " ".join(result)
